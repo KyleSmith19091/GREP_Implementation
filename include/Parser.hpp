994774b888c020cbf61f0834c1a5c4fb6f002a44
Kyle Smith <sKorpion19091@gmail.com>
2021-08-10 10:48:06 +0200

    ⚡️ Added command line parser

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -5,27 +5,17 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
-namespace CommandlineParser {
-    void _tokenise(const int& argc, const char* (argv)[], std::vector<std::string>& tokens) {
-        for(int i = 0; i < argc; i++){
-            tokens[i] = argv[i];
-        }
-    }
+namespace Commandline {
+    class Parser {
+        private:
+            static int argc;
+            static const char* (&argv)[];
+            void tokenise();
 
-    std::pair<std::string,std::string> parseArgs(const int& argc, const char* (argv)[]) {
-        if (argc >= 3) { // Check if input is valid first
-
-            std::vector<std::string> tokens(argc);
-            _tokenise(argc, argv, tokens);
-
-            return std::make_pair(argv[1], argv[2]);
-
-        } else { // Incorrect number of inputs
-            std::cerr << "Incorrect Number of Args Provided\n";
-            return std::make_pair("","");
-        }
-        return std::make_pair("","");
-    }
+        public: 
+            Parser();
+            static std::pair<std::string,std::string> parseArgs(const int& argc, const char* argv[]);
+    };
 };
     
 #endif

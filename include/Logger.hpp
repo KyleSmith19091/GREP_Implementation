d1c3a6e1338fce736cf5925b680b860219f261c7
Kyle Smith <sKorpion19091@gmail.com>
2021-08-12 13:15:29 +0200

    ⚡️ Added highlight text to logger

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -1,13 +1,23 @@
 #include <string>
 #include <ostream>
+#include <vector>
+#include <iostream>
 
 #ifndef LOGGER_HPP
 #define LOGGER_HPP
 
+#include "./ANSIColorTable.h"
+
 namespace Commandline {
     class Logger {
+        private:
+            static const std::string createHighlightedString(const std::string& ansiColor, const std::string& str) noexcept {
+                return ansiColor + str + reset;
+            };
+
         public:
             static std::ostream& write(std::ostream&, const std::string&) noexcept;
+            static void displayHighlightText(std::ostream& os, std::string&, const std::string&, std::vector<size_t>&) noexcept; 
     };
 }
 
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -3,3 +3,17 @@
 std::ostream& Commandline::Logger::write(std::ostream& os, const std::string& data) noexcept {
     return os << data;    
 }
+
+void Commandline::Logger::displayHighlightText(std::ostream& os, std::string& str, const std::string& pattern, std::vector<size_t>& indices) noexcept {
+    const int ansiLength = getLengthOfANSICode(REGULAR_TEXT_LEN);
+    const int patternSize = pattern.size();
+    const std::string highlightText = createHighlightedString(RED,pattern);
+
+    int offset = 0;
+    for (size_t& i : indices) {
+        str.replace(i + offset,patternSize,highlightText);
+        offset += ansiLength;
+    }
+
+    Commandline::Logger::write(os, str + "\n");
+}
